404e81efc2189071fdbe2ea5d0ba808279d5eb5a
alizarda <134419033+alizarda@users.noreply.github.com>
2023-06-15 17:01:02 -0400

    Update gradebook.cpp
    The skeleton code for gradebook.cpp.

--- gradebook.cpp
+++ gradebook.cpp
@@ -1,1 +1,16 @@
+#include "gradebook.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+//The default unparameterized constructor.
+Gradebook::Gradebook(){
+
+}
+
+
+//The destructor is unncessary, since we aren't allocating any dynamic memory on the heap.
+Gradebook::~Gradebook(){
+
+}
 

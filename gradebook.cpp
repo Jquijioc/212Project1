7a96dc15d4b5093c6ca2f96c66f9a6a5ce29d45b
jonathanok1 <113383828+jonathanok1@users.noreply.github.com>
2023-06-20 18:31:17 -0400

    Update gradebook.cpp
    Gets the average of each assignment weight grade
    math will need to be done in their respective functions if needed

--- gradebook.cpp
+++ gradebook.cpp
@@ -14,3 +14,45 @@ Gradebook::~Gradebook(){
 
 }
 
+//Finds the average ****FOR EACH**** assignment in all categorys
+std::vector<float> Gradebook::GetWeightGrades() {
+    std::vector<float> allGradesWeighted;
+
+    // Check if there are any lab grades
+    if (!labGrades.empty()) {
+        float totalLabWeighted = 0;
+        for (int i = 0; i < labGrades.size(); i++) {
+            totalLabWeighted += (labGrades[i] / 200.0) * 0.20;
+        }
+        allGradesWeighted.push_back(totalLabWeighted);
+    }
+
+    // Check if there are any assignment grades
+    if (!assignmentGrades.empty()) {
+        float totalAssignmentWeighted = 0;
+        for (int i = 0; i < assignmentGrades.size(); i++) {
+            totalAssignmentWeighted += (assignmentGrades[i] / 200.0) * 0.20;
+        }
+        allGradesWeighted.push_back(totalAssignmentWeighted / assignmentGrades.size());
+    }
+
+    // Check if there are any project grades
+    if (!projectGrades.empty()) {
+        float totalProjectWeighted = 0;
+        for (int i = 0; i < projectGrades.size(); i++) {
+            totalProjectWeighted += (projectGrades[i] / 500.0) * 0.50;
+        }
+        allGradesWeighted.push_back(totalProjectWeighted / projectGrades.size());
+    }
+
+    // Check if there are any exam grades
+    if (!examGrades.empty()) {
+        float totalExamWeighted = 0;
+        for (int i = 0; i < examGrades.size(); i++) {
+            totalExamWeighted += (examGrades[i] / 100.0) * 0.10;
+        }
+        allGradesWeighted.push_back(totalExamWeighted / examGrades.size());
+    }
+    return allGradesWeighted;
+}
+

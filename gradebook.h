aaa00d9d6174f591b2f64f02489955026ed53641
alizarda <134419033+alizarda@users.noreply.github.com>
2023-06-23 13:27:40 -0400

    Update gradebook.h
    Added private variables for Gradebook constructor.

--- gradebook.h
+++ gradebook.h
@@ -3,8 +3,15 @@
 
 class Gradebook{
     private:
+        std::vector<int> labGrades;
+        std::vector<int> assignmentGrades;
+        std::vector<int> projectGrades;
+        std::vector<int> examGrades;
+        int totalGrade;
+        std::vector<std::string> Course;
     public:
         Gradebook();
+        Gradebook(std::vector<int> labGrades, std::vector<int> assignmentGrades, std::vector<int> projectGrades, std::vector<int> examGrades, int totalGrade);
         ~Gradebook();
         void printGrades();
         void getCategoryTotal();

56b2cfd7394d03dae801b67ba01152f72fe9d59d
alizarda <134419033+alizarda@users.noreply.github.com>
2023-06-25 15:06:01 -0400

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -2,23 +2,25 @@
 #include <vector>
 
 class Gradebook{
-private:
-    std::vector<int> labGrades;
-    std::vector<int> assignmentGrades;
-    std::vector<int> projectGrades;
-    std::vector<int> examGrades;
-    std::vector<std::string> Course;
-public:
-    Gradebook();
-    Gradebook(std::vector<int> labGrades, std::vector<int> assignmentGrades, std::vector<int> projectGrades, std::vector<int> examGrades);
-    ~Gradebook();
-    void printGrades(int category);
-    void getIndividual(int category);
-    void respectiveGrades();
-    void getCategory();
-    void getCategoryTotal();
-    int getCategoryNumber();
-    void getCourseTotal();
-    void getAll();
-    std::vector<float> GetWeightGrades();
+    private:
+        std::vector<int> labGrades;
+        std::vector<int> assignmentGrades;
+        std::vector<int> projectGrades;
+        std::vector<int> examGrades;
+        std::vector<std::string> Course;
+    public:
+        Gradebook();
+        Gradebook(std::vector<int> labGrades, std::vector<int> assignmentGrades, std::vector<int> projectGrades, std::vector<int> examGrades);
+        ~Gradebook();
+        void printGrades(int category);
+        int getCategoryNumber();
+        void getIndividual(int category);
+        void getCategory();
+        void getCategoryTotal();
+        void getCourseTotal();
+        void getAll();
+        void print(int cat, int totalCat, int totalCatPoints);
+        float total(int cat);
+        std::vector<float> totalGrade(int cat);
+        std::vector<float> GetWeightGrades();
 };
